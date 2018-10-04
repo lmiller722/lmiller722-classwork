a7ba5932b9ec78dda2356af056931c8ca255de0d
MCC Local Administrator <lmiller722@gmail.com>
2018-10-04 18:15:10 -0400

    final2

---
+++ Quiz1/HolidayChallenge.js
@@ -0,0 +1,15 @@
+var milesRun = 200000
+var firstPay = .02
+var secondPay = .04
+
+
+var firstHalf = (milesRun / 2) * firstPay
+
+var secondHalf = (milesRun / 2) * secondPay
+
+var finalPay = firstHalf + secondHalf
+
+
+
+
+console.log("I will raise $" + finalPay + " for charity this year!")
